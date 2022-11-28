65ec56d40f5accad669991efe0bc3fe3a24c44df
lswatson16 <lauralynw16@gmail.com>
2022-11-28 18:32:28 -0500

    Move key attribute to top level container

--- src/App.js
+++ src/App.js
@@ -26,9 +26,9 @@ const App = () => {
   return (
     <div className='categories-container'>
       {categories.map(({ title, id }) => (
-        <div className='category-container'>
+        <div className='category-container' key={id}>
           {/* <img /> */}
-          <div className='category-body-container' key={id}>
+          <div className='category-body-container'>
             <h2>{title}</h2>
             <p>Show Now</p>
           </div>
