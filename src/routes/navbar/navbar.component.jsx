cc77238dbecae722013894f337f4e7d6c50f5f31
lswatson16 <lauralynw16@gmail.com>
2022-12-07 23:33:33 -0500

    Add styling and logo to NavBar component; Change default styling
    Start stying the navigation bar; Change default styling of links; Apply border box to all elements

--- src/routes/navbar/navbar.component.jsx
+++ src/routes/navbar/navbar.component.jsx
@@ -1,12 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import './navbar.styles.scss';
 
 const NavBar = () => {
   return (
     <Fragment>
       <div className='navigation'>
         <Link className='logo-container' to='/'>
-          <div>Logo</div>
+          <CrownLogo className='logo'/>
         </Link>
         <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>SHOP</Link>
