204031a0d2c53b01fcb00c47bd9fdbd6d5a95245
lswatson16 <lauralynw16@gmail.com>
2023-01-15 17:34:57 -0500

    Increment the checkout item; add basic html structure and style to checkout page

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,11 +3,43 @@ import { CartContext } from '../../contexts/cart.context';
 import './checkout.styles.scss';
 
 const Checkout = () => {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, addItemToCart } = useContext(CartContext);
 
   return (
     <div className='checkout-container'>
       <p>Checkout Page</p>
+      <div>
+        {
+          cartItems.map(cartItem => {
+            const { id, name, quantity, price, imageUrl } = cartItem;
+            return (
+              <div className='checkout-items-container' key={id}>
+                <img src={imageUrl} alt={`${name}`} />
+                <div className='checkout-details'>
+                  <h2>{name}</h2>
+                  <div className='quanitity-container'>
+                    <span
+                      className='quantity-btn'
+                      onClick={() => console.log('decrease')}
+                    >
+                      decrement
+                    </span>
+                    <span>{quantity}</span>
+                    <span
+                      className='quantity-btn'
+                      onClick={() => addItemToCart(cartItem)}
+                    >
+                      increment
+                    </span>
+                  </div>
+                  <span>{price}</span>
+                  <span onClick={() => console.log('remove')}>Remove</span>
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
     </div>
   );
 };
