e042ad9268ce4953df867f030af78eb6c3a2c688
lswatson16 <lauralynw16@gmail.com>
2023-01-14 21:17:17 -0500

    Track and display the accurate cart count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as ShoppingCartIcon } from '../../assets/shopping-bag.sv
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
   
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
@@ -14,7 +14,7 @@ const CartIcon = () => {
         className='shopping-icon'
         onClick={toggleIsCartOpen}
       />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   );
 };
