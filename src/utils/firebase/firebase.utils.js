b7e84c81d2fe814ffbeb8d196f4916167be722a7
lswatson16 <lauralynw16@gmail.com>
2023-01-16 23:41:04 -0500

    Create a new collection called categories; Set new doc refs
    Add a batch to set the new document references in the new collection called categories. The doc refs reflect the different categories. Also removed hard coded shop-data json file.

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,9 +6,16 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged 
+  onAuthStateChanged
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { 
+  getFirestore, 
+  doc, 
+  getDoc, 
+  setDoc, 
+  collection, 
+  writeBatch, 
+} from 'firebase/firestore';
 
 // Web app's Firebase configuration
 const firebaseConfig = {
@@ -35,6 +42,19 @@ provider.setCustomParameters({
  
  // instantiate Firestore database
  export const db = getFirestore();
+
+ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+   const collectionRef = collection(db, collectionKey);
+   const batch = writeBatch(db);
+
+   objectsToAdd.forEach(object => {
+     const docRef = doc(collectionRef, object.title.toLowerCase());
+     batch.set(docRef, object);
+   });
+
+   await batch.commit();
+   console.log('done');
+ };
 
  export const createUserDocumentFromAuth = async (userAuth, additionalInfo = {}) => {
    if (!userAuth) return;
