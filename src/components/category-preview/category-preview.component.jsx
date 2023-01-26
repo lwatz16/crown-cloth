8a6d3203b861b0db5ac487695e11b328844cf165
lswatson16 <lauralynw16@gmail.com>
2023-01-26 17:18:21 -0500

    Create clickable links on shop page

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,19 +1,21 @@
-import './category-preview.styles.scss';
+import { Link } from 'react-router-dom';
 import ProductCard from '../product-card/product-card.component';
+import './category-preview.styles.scss';
 
 const CategoryPreview = ({ title, products }) => {
-  return(
+  return (
     <div className='category-preview-container'>
       <h2>
-        <span>{title.toUpperCase()}</span>
+        <Link className='title' to={title}>
+          {title.toUpperCase()}
+        </Link>
       </h2>
       <div className='preview'>
-        {
-          products
-            .filter((_, idx) => idx < 4)
-            .map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
+        {products
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
       </div>
     </div>
   );
