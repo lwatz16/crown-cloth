804b30cd0abc6391984a62d95121bfd0d5e16a34
lswatson16 <lauralynw16@gmail.com>
2023-01-27 00:08:10 -0500

    Implement styled components in Directory, ProductCard, SignIn, and SignUp
    Convert SCSS files to JS styled components; Remove Link import from react-router in CategoryPreview

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -5,7 +5,7 @@ import {
   signInAuthUserWithEmailAndPassword,
   signInWithGooglePopup,
 } from '../../utils/firebase/firebase.utils';
-import './sign-in-form.styles.scss';
+import { SignInContainer, ButtonsContainter } from './sign-in-form.styles';
 
 const defaultFormFields = {
   email: '',
@@ -49,7 +49,7 @@ const SignInForm = () => {
   };
 
   return (
-    <div className='sign-in-container'>
+    <SignInContainer>
       <h2>I already have an account</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -70,14 +70,14 @@ const SignInForm = () => {
           name='password'
           value={password}
         />
-        <div className='buttons-container'>
+        <ButtonsContainter>
           <Button type='submit'>Sign In</Button>
           <Button type='button' buttonType='google' onClick={signInWithGoogle}>
             Google Sign In
           </Button>
-        </div>
+        </ButtonsContainter>
       </form>
-    </div>
+    </SignInContainer>
   );
 };
 
