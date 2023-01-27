0166e85c01508040463c50ae3a0ba736d85eec87
lswatson16 <lauralynw16@gmail.com>
2023-01-27 18:36:53 -0500

    Create and implement styled button component

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import { ProductCardContainer, Footer, Name, Price } from './product-card.styles';
 
 const ProductCard = ({ product }) => {
@@ -17,7 +17,7 @@ const ProductCard = ({ product }) => {
         <Price className='price'>{price}</Price>
       </Footer>
       <Button 
-        buttonType='inverted' 
+        buttonType={BUTTON_TYPE_CLASSES.inverted}
         onClick={addProductToCart}
       >
         Add to cart
