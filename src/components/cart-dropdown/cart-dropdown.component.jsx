d2e6976c64702ff4baf88e5015add5e79e4ab70d
lswatson16 <lauralynw16@gmail.com>
2023-01-27 19:09:18 -0500

    Add empty message; convert to styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-import './cart-dropdown.styles.scss';
+import { CartDropdownContainer, CartItems, EmptyMessage } from './cart-dropdown.styles';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
@@ -14,14 +14,20 @@ const CartDropdown = () => {
   };
 
   return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'>
-        {cartItems.map((item) => (
+    <CartDropdownContainer>
+      <CartItems>
+        {
+          cartItems.length ? (cartItems.map((item) => (
           <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
-      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
+          ))) : (
+            <EmptyMessage className='empty-message'>Your cart is empty</EmptyMessage>
+          )   
+        }
+      </CartItems>
+      <Button onClick={goToCheckoutHandler}>
+        GO TO CHECKOUT
+      </Button>
+    </CartDropdownContainer>
   );
 };
 
